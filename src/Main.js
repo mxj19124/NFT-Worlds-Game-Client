481660463da5bb5948011c23da50db4552b2e47e
Braydon Batungbacal <me@braydonb.com>
2021-12-10 17:15:05 -0800

    Began work on basic screen navigation.

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import { View, Image, StyleSheet } from 'react-native';
+import { View, Animated, StyleSheet } from 'react-native';
 import * as Font from 'expo-font';
 import { BackgroundVideo } from './components';
 
 import { LoginScreen, HomeScreen, SettingsScreen } from './screens';
 
 export default class Main extends Component {
+  state = {
+    screen: 'login',
+    opacityAnimated: new Animated.Value(0),
+  }
+
   componentDidMount() {
     Font.loadAsync({
       Regular: require('./assets/fonts/Montserrat-Medium.ttf').default,
@@ -13,12 +18,28 @@ export default class Main extends Component {
       Bold: require('./assets/fonts/Montserrat-Bold.ttf').default,
       BoldItalic: require('./assets/fonts/Montserrat-BoldItalic.ttf').default,
     });
+
+    this._fade(true, 2000);
+  }
+
+  _fade = (fadeIn, durationOverride) => {
+    Animated.timing(this.state.opacityAnimated, {
+      toValue: fadeIn ? 1 : 0,
+      duration: durationOverride || 750,
+      useNativeDriver: true,
+    }).start();
   }
 
   render() {
+    const { screen, opacityAnimated } = this.state;
+
     return (
       <View style={styles.container}>
-        <SettingsScreen />
+        <Animated.View style={[ styles.fadeContainer, { opacity: opacityAnimated } ]}>
+          {screen === 'login' && (<LoginScreen />)}
+          {screen === 'home' && (<HomeScreen />)}
+          {screen === 'settings' && (<SettingsScreen />)}
+        </Animated.View>
 
         <BackgroundVideo />
       </View>
@@ -28,13 +49,11 @@ export default class Main extends Component {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  fadeContainer: {
     alignItems: 'center',
     flex: 1,
     height: '100%',
-  },
-  logoImage: {
-    width: 300,
-    height: 44,
-    marginTop: 50,
   },
 });
