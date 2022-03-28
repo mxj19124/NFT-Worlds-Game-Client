9186253cf6de7a5b74274e34f83039128ad5c538
Jack Baron <jackmbaron@gmail.com>
2022-03-28 13:58:34 +0100

    ensure user is still valid on init

--- src/renderer/state/init.ts
+++ src/renderer/state/init.ts
@@ -1,6 +1,20 @@
 import { type Dispatch } from 'react'
+import { refresh, validate } from '../ipc/auth'
 import { type Action, type State } from './reducer'
 
 export const init = async (state: State, dispatch: Dispatch<Action>) => {
-  // TODO
+  if (state.user) {
+    const isValid = await validate(state.user)
+    if (!isValid) {
+      dispatch({ type: 'setStatus', value: 'authenticating' })
+      try {
+        const profile = await refresh(state.user)
+        dispatch({ type: 'setUser', value: profile })
+      } catch {
+        dispatch({ type: 'clearUser' })
+      }
+    }
+  }
+
+  dispatch({ type: 'setStatus', value: 'idle' })
 }
