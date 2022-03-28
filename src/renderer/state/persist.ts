8ab98be637bede2bc24318a1c201270710c838f7
Jack Baron <jackmbaron@gmail.com>
2022-03-28 13:41:33 +0100

    add user to state

--- src/renderer/state/persist.ts
+++ src/renderer/state/persist.ts
@@ -2,14 +2,18 @@ import Store from 'electron-store'
 import { type State } from './reducer'
 
 const store = new Store<PersistentStore>()
-interface PersistentStore {}
+interface PersistentStore {
+  user: State['user']
+}
 
 export const initState: (initialState: State) => State = initialState => {
   return {
     ...initialState,
+    user: store.get('user') ?? initialState.user,
   }
 }
 
 export const persist = (state: State) => {
-  // TODO: Persist keys
+  if (state.user) store.set('user', state.user)
+  else store.delete('user')
 }
--- src/renderer/state/reducer.ts
+++ src/renderer/state/reducer.ts
@@ -1,18 +1,26 @@
+import { type profile as Profile } from 'msmc'
 import { type Reducer } from 'react'
 
 export interface State {
-  // TODO
+  user: Profile | undefined
 }
 
-export type Action = { type: string; value: unknown }
+export type Action = { type: 'setUser'; value: Profile } | { type: 'clearUser' }
 
 export const reducer: Reducer<State, Action> = (previousState, action) => {
   switch (action.type) {
+    case 'setUser': {
+      return { ...previousState, user: action.value }
+    }
+
+    case 'clearUser':
+      return { ...previousState, user: undefined }
+
     default:
       throw new Error('Invalid Action')
   }
 }
 
 export const initialState: State = {
-  // TODO
+  user: undefined,
 }
