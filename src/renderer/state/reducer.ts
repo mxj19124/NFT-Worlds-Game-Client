4beca3a017643c5e019eb7a5323988dfc8856fad
Jack Baron <jackmbaron@gmail.com>
2022-03-28 13:45:06 +0100

    add status to state

--- src/renderer/state/reducer.ts
+++ src/renderer/state/reducer.ts
@@ -1,14 +1,23 @@
 import { type profile as Profile } from 'msmc'
 import { type Reducer } from 'react'
 
+type Status = 'init' | 'idle' | 'authenticating' | 'gameRunning'
 export interface State {
+  status: Status
   user: Profile | undefined
 }
 
-export type Action = { type: 'setUser'; value: Profile } | { type: 'clearUser' }
+export type Action =
+  | { type: 'setStatus'; value: Status }
+  | { type: 'setUser'; value: Profile }
+  | { type: 'clearUser' }
 
 export const reducer: Reducer<State, Action> = (previousState, action) => {
   switch (action.type) {
+    case 'setStatus': {
+      return { ...previousState, status: action.value }
+    }
+
     case 'setUser': {
       return { ...previousState, user: action.value }
     }
@@ -22,5 +31,6 @@ export const reducer: Reducer<State, Action> = (previousState, action) => {
 }
 
 export const initialState: State = {
+  status: 'init',
   user: undefined,
 }
