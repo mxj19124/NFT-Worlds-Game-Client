70b422bf7bc748a437abeeb6a59c9b4a01c43d93
Jack Baron <jackmbaron@gmail.com>
2022-03-28 16:21:08 +0100

    move minecraft root inside app root

--- src/main/lib/env.ts
+++ src/main/lib/env.ts
@@ -1,3 +1,11 @@
+import { app } from 'electron'
+import { join as joinPath } from 'path'
 import process from 'process'
 
 export const isDevelopment = process.env.NODE_ENV !== 'production'
+
+const dataDir = '.nftworlds'
+const appData = app.getPath('appData')
+export const APP_ROOT = isDevelopment
+  ? joinPath('.', dataDir)
+  : joinPath(appData, dataDir)
