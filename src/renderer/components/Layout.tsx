bd16b994db176bdfab0be979b603e59e2b972500
Jack Baron <jackmbaron@gmail.com>
2022-03-28 21:58:29 +0100

    start working on new UI

--- src/renderer/components/Layout.tsx
+++ src/renderer/components/Layout.tsx
@@ -1,6 +1,7 @@
 import React, { type FC } from 'react'
 import styled from 'styled-components'
-import Logo from '../assets/images/logo.png'
+import LogoImage from '../assets/images/logo.png'
+import { useStore } from '../hooks/useStore'
 
 const Container = styled.div`
   width: 100%;
@@ -11,9 +12,38 @@ const Container = styled.div`
   align-items: center;
 `
 
-const Image = styled.img`
-  width: 40%;
-  margin-bottom: 16px;
+const TopBar = styled.div`
+  width: 100%;
+  height: 48px;
+  display: flex;
+  margin-top: 30px;
+`
+
+const IconsContainer = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  margin-left: 30px;
+`
+
+const LogoContainer = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const Logo = styled.img`
+  width: 200px;
+  height: auto;
+`
+
+const SettingsContainer = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  margin-right: 30px;
 `
 
 const Children = styled.div`
@@ -21,10 +51,22 @@ const Children = styled.div`
   flex-grow: 1;
 `
 
-export const Layout: FC = ({ children }) => (
-  <Container>
-    <Image src={Logo} />
+export const Layout: FC = ({ children }) => {
+  const { state } = useStore()
 
-    <Children>{children}</Children>
-  </Container>
-)
+  return (
+    <Container>
+      <TopBar>
+        <IconsContainer>icons</IconsContainer>
+
+        <LogoContainer>
+          <Logo src={LogoImage} />
+        </LogoContainer>
+
+        <SettingsContainer>settings</SettingsContainer>
+      </TopBar>
+
+      <Children>{children}</Children>
+    </Container>
+  )
+}
