d229fb6a160edc69490ed155bff450e856491d60
Jack Baron <jackmbaron@gmail.com>
2022-03-29 03:51:21 +0100

    convert login button to a dumb component

--- src/renderer/components/LoginMicrosoftButton.tsx
+++ src/renderer/components/LoginMicrosoftButton.tsx
@@ -1,33 +1,13 @@
-import { type update as Update } from 'msmc'
-import React, {
-  type FC,
-  useCallback,
-  useEffect,
-  useMemo,
-  useState,
-} from 'react'
+import React, { type FC, useCallback } from 'react'
 import styled from 'styled-components'
 import SVG from '../assets/svg/microsoft-login.svg'
-import { useStore } from '../hooks/useStore'
-import { login, loginEvents } from '../ipc/auth'
 
-const Container = styled.div`
+const Image = styled.img<Props>`
   width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`
-
-interface ImageProps {
-  disabled: boolean
-}
-
-const Image = styled.img<ImageProps>`
-  width: 300px;
   box-shadow: 0 4px 8px 0px #000000a6;
   transition: transform 0.2s ease;
 
-  opacity: ${props => (props.disabled ? '0.5' : '1')};
+  opacity: ${props => (props.disabled ? '0.7' : '1')};
   cursor: ${props => (props.disabled ? 'initial' : 'pointer')};
 
   &:hover {
@@ -35,65 +15,17 @@ const Image = styled.img<ImageProps>`
   }
 `
 
-interface ProgressProps {
-  progress: number | undefined
+interface Props {
+  disabled?: boolean
+  onClick: () => void
+
+  children?: never
 }
 
-const Progress = styled.div<ProgressProps>`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: ${props => props.progress ?? 0}%;
-  height: 5px;
+export const LoginMicrosoftButton: FC<Props> = ({ disabled, onClick }) => {
+  const handleClick = useCallback(() => {
+    if (!disabled) onClick()
+  }, [disabled, onClick])
 
-  background-color: #0084ff;
-`
-
-export const LoginMicrosoftButton: FC<{ children?: never }> = () => {
-  const { state, dispatch } = useStore()
-  const disabled = useMemo<boolean>(
-    () => state.status === 'authenticating',
-    [state]
-  )
-
-  const [progress, setProgress] = useState<number | undefined>()
-  const [status, setStatus] = useState<string | undefined>()
-
-  const onUpdate = useCallback((update: Update) => {
-    if (update.percent) setProgress(update.percent)
-    if (update.data) setStatus(update.data)
-  }, [])
-
-  useEffect(() => {
-    loginEvents.addListener('update', onUpdate)
-
-    return () => {
-      loginEvents.removeListener('update', onUpdate)
-    }
-  }, [onUpdate])
-
-  const handleLogin = useCallback(() => {
-    dispatch({ type: 'setStatus', value: 'authenticating' })
-
-    void login()
-      .then(profile => {
-        dispatch({ type: 'setUser', value: profile })
-      })
-      .catch(console.error)
-      .finally(() => {
-        setProgress(undefined)
-        setStatus(undefined)
-
-        dispatch({ type: 'setStatus', value: 'idle' })
-      })
-  }, [dispatch])
-
-  return (
-    <Container>
-      <p>{status}</p>
-      <Image disabled={disabled} src={SVG} onClick={handleLogin} />
-
-      <Progress progress={progress} />
-    </Container>
-  )
+  return <Image disabled={disabled} src={SVG} onClick={handleClick} />
 }
