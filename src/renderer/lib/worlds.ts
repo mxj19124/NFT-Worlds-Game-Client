8cb388f1be81d712daa81839d37f5e556ba03eeb
Jack Baron <jackmbaron@gmail.com>
2022-03-30 12:39:53 +0100

    parse newlines correctly

--- src/renderer/lib/worlds.ts
+++ src/renderer/lib/worlds.ts
@@ -3,11 +3,15 @@ import axios from 'axios'
 type World = NFTWorlds.World
 export const fetchWorlds: () => Promise<World[] | Error> = async () => {
   try {
-    const resp = await axios.get<World[]>(
+    const { data: worlds } = await axios.get<World[]>(
       'https://status-api.nftworlds.com/latest'
     )
 
-    return resp.data
+    for (const world of worlds) {
+      world.description = world.description.replace(/\\n/g, '\n').trim()
+    }
+
+    return worlds
   } catch (error: unknown) {
     if (error instanceof Error) return error
     throw error
