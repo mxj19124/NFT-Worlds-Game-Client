a930316c2b5da02325b467935ee8bc69ff2bf328
Jack Baron <jackmbaron@gmail.com>
2022-04-04 17:24:22 +0100

    unmount on HMR

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,17 +1,16 @@
 import React from 'react'
-import { createRoot, type Root } from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import { App } from './App'
 import { Provider } from './components/Store'
 import './assets/fonts/OpenSans.css'
 
 if (module.hot) {
+  module.hot.dispose(() => root?.unmount())
   module.hot.accept()
 }
 
 const container = document.querySelector('#app')
-
-let root: Root | undefined
-if (root === undefined) root = createRoot(container!)
+const root = createRoot(container!)
 
 root.render(
   <React.StrictMode>
