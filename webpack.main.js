7edf783c6d647a55c31bab61e24e4272d4d361b6
Jack Baron <jackmbaron@gmail.com>
2022-04-07 16:42:11 +0100

    only copy dev app update on dev builds

--- webpack.main.js
+++ webpack.main.js
@@ -1,11 +1,18 @@
 /* eslint-disable */
 const CopyPlugin = require('copy-webpack-plugin')
 
-module.exports = config => {
-  const copy = new CopyPlugin({
-    patterns: [{ from: './dev-app-update.yml', to: '.' }],
-  })
+console.log(process.env.NODE_ENV)
 
-  config.plugins.push(copy)
+module.exports = config => {
+  const isDev = config.mode !== 'production'
+
+  if (isDev) {
+    const copy = new CopyPlugin({
+      patterns: [{ from: './dev-app-update.yml', to: '.' }],
+    })
+
+    config.plugins.push(copy)
+  }
+
   return config
 }
