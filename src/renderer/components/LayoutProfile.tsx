9b7c25b65d28623592b51a66c85a95e685a72485
Jack Baron <jackmbaron@gmail.com>
2022-04-11 15:35:01 +0100

    periodically update WRLD balance

--- src/renderer/components/LayoutProfile.tsx
+++ src/renderer/components/LayoutProfile.tsx
@@ -1,6 +1,7 @@
-import React, { type FC } from 'react'
+import React, { type FC, useEffect } from 'react'
 import styled from 'styled-components'
 import { useStore } from '../hooks/useStore'
+import { authGetWallets } from '../ipc/nftw'
 import { PlayerProfile } from './PlayerProfile'
 
 const SettingsContainer = styled.div`
@@ -12,8 +13,23 @@ const SettingsContainer = styled.div`
 `
 
 export const LayoutProfile: FC = () => {
-  const { state } = useStore()
-  console.log(state)
+  const { state, dispatch } = useStore()
+
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      if (!state.user) return
+      if (!state.wallets) return
+
+      console.log('updating wallet info')
+      // @ts-expect-error Untyped Property
+      const mcToken = state.user._msmc.mcToken as string
+      const wallets = await authGetWallets(mcToken, state.wallets?.nftwToken)
+
+      dispatch({ type: 'setWallets', value: wallets })
+    }, 30 * 1000)
+
+    return () => clearInterval(interval)
+  }, [state.user, state.wallets, dispatch])
 
   return (
     <SettingsContainer>
